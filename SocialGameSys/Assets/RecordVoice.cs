e4e89ba1949f2786cc4bcee0019b1c46ac17d608
Dylan Alter <alterdylan@gmail.com>
2018-04-08 21:42:50 -0400

    game

---
+++ SocialGameSys/Assets/RecordVoice.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecordVoice : MonoBehaviour {
+
+    AudioSource aud;
+
+    // Use this for initialization
+    void Start () {
+         aud = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            aud.clip = Microphone.Start("Microphone", true, 200, 44100);
+            aud.PlayDelayed(0.25f);
+        }
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            aud.Pause();
+        }
+
+	}
+}
